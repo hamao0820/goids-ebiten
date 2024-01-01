928095e1a6176d54ac32e2ee58e79b97b95205b9
浜田　俊祐 <hamashun0820@icloud.com>
2024-01-01 21:39:49 +0900

    add: Add window customization options

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func main() {
 	g := goids.NewGame()
 	ebiten.SetWindowSize(goids.Width, goids.Height)
 	ebiten.SetWindowTitle("Goids")
+	ebiten.SetWindowFloating(true)
+	ebiten.SetWindowDecorated(false)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetWindowMousePassthrough(true)
 	op := &ebiten.RunGameOptions{}
 	op.ScreenTransparent = true
 	if err := ebiten.RunGameWithOptions(g, op); err != nil {
