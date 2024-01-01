a81f35a457b3cb8d339963e4a0b0e008e121190e
浜田　俊祐 <hamashun0820@icloud.com>
2024-01-01 14:53:59 +0900

    add: Add gopher image to game

--- goids/game.go
+++ goids/game.go
@@ -1,11 +1,26 @@
 package goids
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
 
 const (
 	Width  = 640
 	Height = 480
 )
+
+var (
+	gopher *ebiten.Image
+)
+
+func init() {
+	var err error
+	gopher, _, err = ebitenutil.NewImageFromFile("assets/images/gopher.png")
+	if err != nil {
+		panic(err)
+	}
+}
 
 type Game struct {
 }
@@ -19,6 +34,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	screen.DrawImage(gopher, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
